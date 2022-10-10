5c82ce82ac41e45fdc15bcc721b1fe0c925b74e9
Михайленко Эдуард Викторович <EMikhailenko@cbgr.ru>
2022-10-10 17:53:03 +0300

    Fill tables 4

---
+++ src/main/java/com/biomechanics/database/FillContactTypes.sql
@@ -0,0 +1,2 @@
+INSERT INTO biomechanics.contact_types (contact_title) VALUES ('mail');
+INSERT INTO biomechanics.contact_types (contact_title) VALUES ('phone');
---
+++ src/main/java/com/biomechanics/services/SectionInfoService.java
@@ -0,0 +1,12 @@
+package com.biomechanics.services;
+
+import com.biomechanics.domain.entities.general.SectionInfosCoach;
+import com.biomechanics.domain.entities.sections.SectionInfo;
+
+import java.util.List;
+
+public interface SectionInfoService<T> extends CrudService<T>{
+    List<SectionInfo> findByCity(String city);
+
+    List<SectionInfo> findByTitle(String title);
+}
