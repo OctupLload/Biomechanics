86ce0b498a077fa108916a2421fdf7f38873091e
OctupLload <80491431+OctupLload@users.noreply.github.com>
2022-10-12 09:24:16 +0300

    Delete src directory

--- src/main/java/com/biomechanics/repository/SectionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.biomechanics.repository;
-
-import com.biomechanics.domain.Section;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SectionRepository extends CrudRepository<Section, Long> {
-}
